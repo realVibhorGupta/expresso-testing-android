a82110cdd29ef60e672b993b9f7311a84e5aedde
Vibhor Gupta <vibhor.88.gupta@gmail.com>
2019-08-05 17:08:57 +0530

    Add files via upload

---
+++ ExpressoTestingDemo/app/src/androidTest/java/www/vibhorgupta/in/expressotestingdemo/MainActivityBasicTest.java
@@ -0,0 +1,26 @@
+package www.vibhorgupta.in.expressotestingdemo;
+
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by vibhorgupta on 9/14/17.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityBasicTest {
+
+//    @Rule public ActivityTestRule<MainActivity> mainActivityActivityTestRule =
+//            new ActivityTestRule<MainActivity>(MainActivity.class);
+
+
+    @Test
+    public void addButton(){
+
+    }
+
+
+}
